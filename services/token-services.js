90dcf7eab108753043a452a40991f1ab3369047a
gdguerrerog <gdguerrerog@unal.edu.co>
2019-10-04 02:05:36 -0500

    :alembic: forgot routes on develop :poop: :see_no_evil:

--- services/token-services.js
+++ services/token-services.js
@@ -2,7 +2,16 @@
 
 var jwt = require('jwt-simple');
 var secret = 'hqQw0P7dUW4Q2TV2J9W9Ig5hF6ks21DASoX8S';
+var moment = require('moment');
 
 function getTokens(user, req) {
-    for (var i of req) console.log(i);
+    var ip = req.connection.remoteAddress;
+
+
+
+
+}
+
+module.exports = {
+    getTokens: getTokens,
 }
