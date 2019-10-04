48c53117312354927a81d30b9547576792067105
icquirogac <icquirogac@unal.edu.co>
2019-10-04 03:23:05 -0500

    :card_file_box: Creando controladores y modelos para agregar usuarios y carros

--- components/user/user.model.js
+++ components/user/user.model.js
@@ -5,7 +5,20 @@ var Schema = mongoose.Schema;
 
 var user = Schema({
     email: String,
-    cryptedPass: String,
+    name: String,
+    password: String,
+    birthdate: Date,
+    avatar: String,
+    cars: [{
+        branch: String,
+        model: Number,
+        lastSoatDate: Date,
+        lastTecDate: Date, // male, female
+        reference: String,
+        license_plate: String,
+        picture: String,    
+        currentKilometer: Number,    
+    }],
 });
 
 module.exports = mongoose.model('user', user);
--- components/user/user.routes.js
+++ components/user/user.routes.js
@@ -5,6 +5,8 @@ var userController = require('./user.controller');
 
 var api = express.Router();
 
+api.post('/add_car', userController.addCar);
 api.post('/logIn', userController.logIn);
+api.put('/create_user', userController.createUser);
 
 module.exports = api;
