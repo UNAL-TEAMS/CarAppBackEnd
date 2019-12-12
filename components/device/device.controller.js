a4b392a46f518764e9b7173a09e8c7ec80deaddd
gdguerrerog <gdguerrerog@unal.edu.co>
2019-12-12 17:16:44 -0500

    :ok_hand::bug::pencil2: tested arduino and solved bugs

--- components/device/device.controller.js
+++ components/device/device.controller.js
@@ -10,9 +10,12 @@ function test(req, res) {
 
 async function km(req, res) {
     res.status(200).send({ ok: 'ok' });
-    const { err, user } = await UserModel.updateOne({ 'cars.license_plate': req.params.plate }, { 'cars.$.currentKilometer': Number(req.params.value) }, { new: true, useFindAndModify: true });
-    if (err) error.handleError(err, 'Error searching user on device.km');
-    console.log('Updated user: ', new Date());
+    //console.log(req.params)
+    UserModel.findOneAndUpdate({ 'cars.license_plate': req.params.plate }, { 'cars.$.currentKilometer': Number(req.params.value) }, { new: true, useFindAndModify: true }, (err, user) => {
+        if (err) error.handleError(err, 'Error searching user on device.km');
+        //console.log('Updated user: ', user);
+    });
+
 }
 
 module.exports = {
